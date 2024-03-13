7e3dfdb95b4f5db86f477bc36820847aa39f7147
lnkarma <ilokenathkarmakar@gmail.com>
2024-03-13 14:49:04 +0530

    add discord login

--- db.ts
+++ db.ts
@@ -1,12 +1,12 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
-import { ExampleTable } from "./lib/db/schema/users";
+// import { ExampleTable } from "./lib/db/schema/users";
 
 // Use this object to send drizzle queries to your DB
 export const db = drizzle(sql);
 // Create a pgTable that maps to a table in your DB
 
-export const getExampleTable = async () => {
-  const selectResult = await db.select().from(ExampleTable);
-  console.log("Results", selectResult);
-};
+// export const getExampleTable = async () => {
+//   const selectResult = await db.select().from(ExampleTable);
+//   console.log("Results", selectResult);
+// };
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,23 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Reddit from "next-auth/providers/reddit";
+import Discord from "next-auth/providers/discord";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-} = NextAuth({ providers: [GitHub] });
+} = NextAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [
+    GitHub,
+    Reddit,
+    Discord({
+      clientId: process.env.AUTH_DISCORD_CLIENT_ID,
+      clientSecret: process.env.AUTH_DISCORD_CLIENT_SECRET,
+    }),
+  ],
+});
